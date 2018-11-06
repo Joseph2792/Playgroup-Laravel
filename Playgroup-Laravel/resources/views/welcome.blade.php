2262c0d8fae864c5285b9069ffaa85ae55642fad
Jose Alfaro <jalfaro@perceptiongrp.com>
2018-11-06 17:34:12 -0300

    fix

--- Playgroup-Laravel/resources/views/welcome.blade.php
+++ Playgroup-Laravel/resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
                     Laravel
                 </div>
             </div>
-
+            <div>
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
